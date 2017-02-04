5dec17407faf2135d3113087620b9481aac9efc1
Tony <tonywang3334@gmail.com>
2017-02-04 20:10:23 +0100

    done!

---
+++ Tutorial/data/menu.js
@@ -0,0 +1,50 @@
+/**
+ * Created by tonyw on 2017-02-04.
+ */
+
+var menu = [
+    {
+        "name": "bear1",
+        "kCal": 200,
+        "lactose": true,
+        "img": "http://i.imgur.com/zmKmgu0.jpg"
+
+    },
+    {
+        "name": "bear2",
+        "kCal": 200,
+        "lactose": true,
+        "img": "http://i.imgur.com/zmKmgu0.jpg"
+
+    },
+    {
+        "name": "bear3",
+        "kCal": 300,
+        "lactose": true,
+        "img": "http://i.imgur.com/zmKmgu0.jpg"
+
+    },
+    {
+        "name": "bear4",
+        "kCal": 400,
+        "lactose": true,
+        "img": "http://i.imgur.com/zmKmgu0.jpg"
+
+    },
+    {
+        "name": "bear5",
+        "kCal": 500,
+        "lactose": true,
+        "img": "http://i.imgur.com/zmKmgu0.jpg"
+
+    },
+    {
+        "name": "bear6",
+        "kCal": 600,
+        "lactose": true,
+        "img": "http://i.imgur.com/zmKmgu0.jpg"
+
+    }
+
+
+];
--- Tutorial/js/script.js
+++ Tutorial/js/script.js
@@ -29,20 +29,52 @@ function Menu(name, kcal, lactose, image) {
     }
 
 }
-
+/*
 function addMenuToDiv(item, index){
+    var currentIndex = index; //index to string
+
     var img = document.createElement("img"); //create img element
     img.setAttribute("src",item.getImage()); //set image source
 
-    //which id????
-    var div = document.getElementById(""); //get the div we want to insert the img
-    div.appendChild(img); //append img to div
-    document.body.appendChild(div); //append div to body
+    var idrow = (index < 3) ? "alcorow" : "nonalcorow"; //alco row or non alco row
+
+    var id = document.getElementById(idrow); //get the div we want to insert the img
+    var mytd = "<td 'id=b" + currentIndex + "'>" + img + "</td>";
+    id.innerHTML += mytd;
+    console.log(id);
+    console.log(idrow);
+    console.log(id.innerHTML);
+
+    //id.appendChild(mytd); //append img to div
+    document.body.appendChild(id); //append div to body
 
     var text = document.getElementById(""); //get id for menu or create?
-    text.innerHTML = text.innerHTML + " "+ item.get(); //add item text to
+    text.innerHTML = text.innerHTML + " " + item.get(); //add item text to
+
+}*/
+
+
+
+function addMenuToDiv(item, index){
+    var currentIndex = index; //index to string
+
+    var img = document.createElement("img"); //create img element
+    img.setAttribute("src",item.getImage()); //set image source
+
+    var idrow = (index < 3) ? "#alcorow" : "#nonalcorow"; //alco row or non alco row
+
+    //var id = document.getElementById(idrow); //get the <tr> we want to insert the img
+    var newtd =  document.createElement("td");
+    newtd.setAttribute("id", index);
+    newtd.appendChild(img); //= "HELLO HELLO HLEOLHEOLHOELOHLEOHLEOLHEOLHOE";//'<img "src=' + item.getImage() + '">';
+    $(idrow).append(newtd);
+
+
+
 
 }
+
+
 
 function displayItems(){
     var menu1 = new Menu("beer", 600, false, "http://i.imgur.com/zmKmgu0.jpg");
@@ -50,7 +82,7 @@ function displayItems(){
     var menu3 = new Menu("water", 0, false, "http://i.imgur.com/zmKmgu0.jpg");
     var menu4 = new Menu("milk", 40, true, "http://i.imgur.com/zmKmgu0.jpg");
     var menu5 = new Menu("vodka", 80, false, "http://i.imgur.com/zmKmgu0.jpg");
-    var menuArray = [menu1, menu2, menu3, menu4, menu5];
+    var menuArray = [menu1, menu2, menu5, menu4, menu3];
 
     menuArray.forEach(addMenuToDiv);
 }
@@ -59,6 +91,38 @@ function indexPageLoaded(){
     displayItems();
 }
 
+function buttonTrigger(){
+    console.log("buttontrigger");
+    $('#myButton').click(function() {
+        console.log("clicker");
+        trigger();
+    });
+
+}
+function trigger(){
+    /*
+    var choco = document.getElementById("choco");
+    var sugar = document.getElementById("sugar");
+
+    var radio = (choco.checked) ? "choco" : "sugar";
+    var dropdown = document.getElementById("watertype");
+    console.log(radio);
+    console.log(dropdown);
+
+    var d = dropdown.options[dropdown.selectedIndex].text;
+    console.log(d);*/
+    console.log("trigger")
+   var radio =  $('input[name=add]:checked', '#radioform').val();
+   var d = $('#watertype option:selected').val();
+
+    alert(radio + d);
+}
+
+
+$(function () {
+    indexPageLoaded();
+    buttonTrigger();
+});
 
 /*
  <body>
